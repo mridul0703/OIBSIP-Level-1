cc14cb4f8238a5c4f8a6d3477c33cd5384b32b4f
Mridul M Kumar <98074317+mridul0703@users.noreply.github.com>
2024-06-05 22:50:51 +0530

    Update script.js

--- OIBSIP Level-1 Task-1/script.js
+++ OIBSIP Level-1 Task-1/script.js
@@ -9,27 +9,31 @@ menu.onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
+
 // Dark Mode
 let darkmode = document.querySelector('#darkmode');
 
+// Set dark mode as default
+document.body.classList.add('active');
+darkmode.classList.replace('bx-moon', 'bx-sun');
+
 darkmode.onclick = () => {
-    if(darkmode.classList.contains('bx-moon')){
-        darkmode.classList.replace('bx-moon','bx-sun');
-        document.body.classList.add('active');
-    }else{
-        darkmode.classList.replace('bx-sun','bx-moon');
+    if (darkmode.classList.contains('bx-sun')) {
+        darkmode.classList.replace('bx-sun', 'bx-moon');
         document.body.classList.remove('active');
+    } else {
+        darkmode.classList.replace('bx-moon', 'bx-sun');
+        document.body.classList.add('active');
     }
 }
 
 // Scroll Reveal
-const sr = ScrollReveal ({
+const sr = ScrollReveal({
     origin: 'top',
     distance: '40px',
     duration: 2000,
     reset: true
 });
-
 
 sr.reveal(`.home-text, .home-img,
             .about-img, .about-text,
@@ -37,4 +41,4 @@ sr.reveal(`.home-text, .home-img,
             .btn, .connect-text,
             .contact-box`, {
     interval: 200
-})
+});
